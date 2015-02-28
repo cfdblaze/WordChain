4ab08ccc1f1dbb02b657940de0c5d32b2276006c
cfdblaze <coreyfdonaldson@gmail.com>
2015-02-28 05:01:43 -0700

    Tester class methods to test the Dictionary class via asserts finished.

--- app/src/main/java/com/example/blaze/wordchain/Tester.java
+++ app/src/main/java/com/example/blaze/wordchain/Tester.java
@@ -1,9 +1,18 @@
 package com.example.blaze.wordchain;
 
+import junit.framework.Assert;
+
 /**
  * Created by eachee on 2/27/2015.
  */
 public class Tester {
     private WordChain wordChain = new WordChain();
-
+    public boolean testXML(String XMLVerify){
+        Assert.assertNotNull(XMLVerify);
+        return true;
+    }
+    public void responseTest(int code) {
+        Assert.assertEquals(200, code);
+        return;
+    }
 }
