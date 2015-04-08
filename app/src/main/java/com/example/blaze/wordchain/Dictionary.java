e4383519b7966caf0cb09700978921c086a5e474
eachee <eliska.achee@gmail.com>
2015-04-08 12:39:26 -0600

    got rid of unused import statements

--- app/src/main/java/com/example/blaze/wordchain/Dictionary.java
+++ app/src/main/java/com/example/blaze/wordchain/Dictionary.java
@@ -1,8 +1,6 @@
 package com.example.blaze.wordchain;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.res.AssetManager;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -10,29 +8,15 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
 import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
-import android.content.res.AssetManager;
 import android.util.Log;
 
 /**
